106f5683a138597349c1be6f012f37f1e4bc9971
Gopishankar <gopi.shanky@gmail.com>
2021-09-18 23:15:14 +0530

    :fire: remove hardcoded api token to make use of environment variable

--- utils/axios/index.ts
+++ utils/axios/index.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   headers: {
-    // "x-api-token": process.env.X_API_TOKEN,
-    "x-api-token": "1575f461e91cef9e7ee6bf31ea6ee22098b2d9bf",
+    "x-api-token": process.env.X_API_TOKEN,
   },
   baseURL: "https://stage.lokalise.com/api2",
 });
